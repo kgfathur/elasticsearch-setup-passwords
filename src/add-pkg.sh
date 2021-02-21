987ba2cc1f5d9f504cef050965a8ffec81e2c471
kgfathur <sahabatsolder@gmail.com>
2021-02-22 01:29:38 +0700

    rename build-env to build.env

--- src/add-pkg.sh
+++ src/add-pkg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source /tmp/src/build-env \
+source /tmp/src/build.env \
 && INSTALL_PKGS=$(cat /tmp/src/pkg-list.txt) \
 && apk add $INSTALL_PKGS \
 && unset $(cat /tmp/src/build.env | cut -d'=' -f1) \
